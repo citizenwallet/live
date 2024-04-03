d3398c633031bfb9538b83f0f72b69b497227d4e
Xavier Damman <xdamman@gmail.com>
2024-04-03 17:29:44 +0200

    fix for gnosis

--- src/app/[chainOrCommunitySlug]/page.tsx
+++ src/app/[chainOrCommunitySlug]/page.tsx
@@ -1,8 +1,10 @@
 "use server";
 
 import MonitorPage from "@/containers/MonitorPage";
-import { ConfigService } from "@citizenwallet/sdk";
 import { Suspense } from "react";
+
+const configUrl =
+  "https://config.internal.citizenwallet.xyz/v3/communities.json";
 
 interface props {
   params: {
@@ -17,8 +19,13 @@ export default async function Page({
   params: { chainOrCommunitySlug: communitySlug },
   searchParams: { accountAddress },
 }: props) {
-  const configService = new ConfigService();
-  const config = await configService.getBySlug(communitySlug);
+  const res = await fetch(
+    `${configUrl}?cacheBuster=${Math.round(new Date().getTime() / 10000)}`
+  );
+  const configs = await res.json();
+  const config = configs.find(
+    (config) => config.community.alias === communitySlug
+  );
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
