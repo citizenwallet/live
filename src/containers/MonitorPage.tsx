aca28fdfa19f4d1ced22c2f797e8929f889931e5
Xavier Damman <xdamman@gmail.com>
2024-06-07 10:57:03 +0200

    fix date picker

--- src/containers/MonitorPage.tsx
+++ src/containers/MonitorPage.tsx
@@ -26,9 +26,12 @@ function formatDateToISO(date: Date) {
   const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-based
   const day = String(date.getDate()).padStart(2, "0");
 
-  // Combine the parts into the desired format
-  const formattedDate = `${year}-${month}-${day}T00:00`;
+  // Extract the hours and minutes from the date
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
 
+  // Combine the parts into the desired format
+  const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}`;
   return formattedDate;
 }
 
