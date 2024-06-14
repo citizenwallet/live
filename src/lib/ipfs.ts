985bbbcc10dbbd05c7d219393b4a613dd8a5e69e
kev-techi <kevin@techi.be>
2024-06-14 12:20:47 +0200

    handle multiple hash formats

--- src/lib/ipfs.ts
+++ src/lib/ipfs.ts
@@ -2,5 +2,6 @@ export const IPFS_BASE_URL = "https://ipfs.internal.citizenwallet.xyz";
 
 export function getUrlFromIPFS(hash: string) {
   if (!hash) return undefined;
+  if (hash.startsWith(IPFS_BASE_URL)) return hash;
   return `${IPFS_BASE_URL}/${hash.replace("ipfs://", "")}`;
 }
