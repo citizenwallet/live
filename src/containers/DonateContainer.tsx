c9c3b95a339b180cfc0e6b9df1c26956be8270f9
Xavier Damman <xdamman@gmail.com>
2024-07-12 19:02:45 +0200

    Revert "take people to install the app"
    This reverts commit 51963b6e011bf64d6ffd084118f87451890fd988.

--- src/containers/DonateContainer.tsx
+++ src/containers/DonateContainer.tsx
@@ -52,14 +52,13 @@ export default function DonateContainer({
   const communitySlug = config?.community.alias;
   const { width, height } = useWindowSize();
   if (!config) return null;
-  // const cwDonateLink = generateReceiveLink(
-  //   "https://app.citizenwallet.xyz",
-  //   accountAddress,
-  //   communitySlug === "regenvillage.wallet.pay.brussels"
-  //     ? "wallet.pay.brussels"
-  //     : communitySlug
-  // );
-  const cwDonateLink = "https://app.citizenwallet.xyz";
+  const cwDonateLink = generateReceiveLink(
+    "https://app.citizenwallet.xyz",
+    accountAddress,
+    communitySlug === "regenvillage.wallet.pay.brussels"
+      ? "wallet.pay.brussels"
+      : communitySlug
+  );
   const redirectUrl = `${process.env.NEXT_PUBLIC_WEBAPP_URL}/${communitySlug}/${accountAddress}/donate`;
   const givethPlugin = getPlugin(config, "giveth");
   const topupPlugin = getPlugin(config, "Top Up");
