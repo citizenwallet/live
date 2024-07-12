e912e70eb983418121e9ccc1fd88ab2705d994e6
Xavier Damman <xdamman@gmail.com>
2024-07-12 12:51:48 +0200

    build fix

--- src/app/fundraiser/[communitySlug]/[accountAddress]/page.tsx
+++ src/app/fundraiser/[communitySlug]/[accountAddress]/page.tsx
@@ -40,7 +40,6 @@ export default async function Page({
         <ThankYouPage
           communityConfig={config}
           accountAddress={accountAddress}
-          title={title}
           collectiveSlug={collectiveSlug}
         />
       )}
